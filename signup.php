134ace4ff9f4aff41e8484c3695ac1d8d55c2503
Hala Ibrahim <halaibrahim867@gmail.com>
2022-10-29 05:44:49 +0200

    edit signup file

--- signup.php
+++ signup.php
@@ -1,3 +1,27 @@
+<?php
+    session_start();
+    include 'connection.php';
+    include 'function.php';
+   
+    if($_SERVER['REQUEST_METHOD']=="POST")
+    {
+        $user_name=$_POST['user_name'];
+        $password=$_POST['password'];
+        if( !empty($user_name ) && !empty($password) && !is_numeric($user_name))
+        {
+            $user_id =random_num(20);
+            $query="insert into users 
+            (user_id,first_name, last_name,email,user_name, password) values ('$user_id', '$first_name', '$last_name ', '$email', '$user_name' , '$password' )";  
+            
+            mysqli_query($con,$query);
+
+            header("Location : login.php");
+            die;
+        }else{
+            echo "Please enter some valid information";
+        }
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
